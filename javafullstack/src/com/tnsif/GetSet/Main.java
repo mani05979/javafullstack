6283dbbd347594483dea97064a8e2e3b756f3bbe
CH Navya <CH Navya@DESKTOP-KAF8MID>
2025-09-20 19:17:45 +0530

    Getter And setter

---
+++ javafullstack/src/com/tnsif/GetSet/Main.java
@@ -0,0 +1,13 @@
+package com.tnsif.GetSet;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person obj= new Person();
+		obj=new Person();
+		System.out.println(obj.getName());
+
+	}
+
+}
---
+++ javafullstack/src/com/tnsif/GetSet/Person.java
@@ -0,0 +1,16 @@
+package com.tnsif.GetSet;
+
+public class Person {
+	//getter
+	private String name;
+	public String getName() {
+		return name;
+		
+	}
+	//setter
+	public void setName(String Mani)
+	{
+		this.name=Mani;
+	}
+
+}
