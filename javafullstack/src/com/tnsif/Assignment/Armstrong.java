6012946e2675e10ca567e2139c73065aecdd5df0
CH Navya <CH Navya@DESKTOP-KAF8MID>
2025-09-20 14:21:33 +0530

    Assignment programs

---
+++ javafullstack/src/com/tnsif/Assignment/Armstrong.java
@@ -0,0 +1,26 @@
+package com.tnsif.Assignment;
+
+public class Armstrong {
+	public static void main(String[] args) {
+	    int n=143;
+	    int temp=n;
+	    int r,sum=0;
+	     while(n>0) {
+	    	 r=n%10;
+	    	 n=n/10;
+	    	 sum=sum+r*r*r;
+	     }
+	     if(temp==sum) {
+				System.out.println("Armstrong number");
+	     }
+				else {
+	    			System.out.println("not an armstrong  number");
+
+
+	    	 
+	     }
+	     
+	     
+		}
+
+}
---
+++ javafullstack/src/com/tnsif/Assignment/EvenorOdd.java
@@ -0,0 +1,14 @@
+package com.tnsif.Assignment;
+
+public class EvenorOdd {
+	public static void main(String[] args) {
+        int m=7;
+        if(m%2==0) {
+        	System.out.println("It is a even number");
+        }
+        else {
+        System.out.println("It is a odd number"); 
+}
+
+}
+}
