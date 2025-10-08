a981e0b42e0ae09d3977980cfa828eb0fd140d82
laxmi <laxmi@gmail.com>
2025-10-08 18:30:49 +0530

    threads

---
+++ javafullstack/src/com/tnsif/threads/Joinmain.java
@@ -0,0 +1,13 @@
+package com.tnsif.threads;
+
+public class Joinmain {
+	public static void main(String[] args) throws InterruptedException {
+		Join j1 = new Join();
+		Join j2 = new Join();
+		j1.start();
+		
+		j1.join();
+		
+		j2.start();
+}
+}
