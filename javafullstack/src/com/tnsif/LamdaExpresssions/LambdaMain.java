fcddaea1b7aaf6d46053ee72c008fdc028fd505d
laxmi <laxmi@gmail.com>
2025-10-08 17:30:45 +0530

    Lambda expressions

---
+++ javafullstack/src/com/tnsif/LamdaExpresssions/LambdaMain.java
@@ -0,0 +1,11 @@
+package com.tnsif.LamdaExpresssions;
+
+public class LambdaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		lambda_in f = (int x)->System.out.println(2*x);
+		f.show(6);
+	}
+
+}
---
+++ javafullstack/src/com/tnsif/LamdaExpresssions/lambda_in.java
@@ -0,0 +1,5 @@
+package com.tnsif.LamdaExpresssions;
+
+public interface lambda_in {
+	void show(int x);
+}
