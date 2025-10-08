8d19ec7177a7b971128c1c1004be239e8ed2fc81
laxmi <laxmi@gmail.com>
2025-10-08 17:51:50 +0530

    Stream Api

---
+++ javafullstack/src/com/tnsif/StreamApi/Product.java
@@ -0,0 +1,18 @@
+package com.tnsif.StreamApi;
+
+public class Product {
+
+		int id;
+		String name;
+		float price;
+		
+		public Product(int id, String name, float price) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.price = price;
+		}
+
+
+	}
+
