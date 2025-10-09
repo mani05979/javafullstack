e7c6301e25d3d56eaea9603517703b704f80e036
laxmi <laxmi@gmail.com>
2025-10-09 14:30:06 +0530

    Jdbc programs

---
+++ javafullstack/src/com/tnsif/JDBC/DeleteStudent.java
@@ -0,0 +1,20 @@
+package com.tnsif.JDBC;
+
+import java.sql.*;
+public class DeleteStudent {
+	private Connection con;
+
+    public DeleteStudent(Connection con) {
+        this.con = con;
+    }
+
+    public void execute(int sid) throws SQLException {
+        String sql = "DELETE FROM student WHERE sid = ?";
+        try (PreparedStatement pst = con.prepareStatement(sql)) {
+            pst.setInt(1, sid);
+            int rows = pst.executeUpdate();
+            System.out.println(rows + " record(s) deleted: SID=" + sid);
+        }
+    }
+
+}
---
+++ javafullstack/src/com/tnsif/JDBC/UpdateStudent.java
@@ -0,0 +1,20 @@
+package com.tnsif.JDBC;
+
+import java.sql.*;
+public class UpdateStudent {
+	private Connection con;
+
+    public UpdateStudent(Connection con) {
+        this.con = con;
+    }
+
+    public void execute(int sid, String sname) throws SQLException {
+        String sql = "UPDATE student SET sname = ? WHERE sid = ?";
+        try (PreparedStatement pst = con.prepareStatement(sql)) {
+            pst.setString(1, sname);
+            pst.setInt(2, sid);
+            int rows = pst.executeUpdate();
+            System.out.println(rows + " record(s) updated: SID=" + sid + " → SNAME=" + sname);
+        }
+    }
+}
